699669b0e08f6a17144898732e50d0d96a395007
aditya060414 <singh.aditya.44618@gmail.com>
2026-04-29 15:27:45 +0530

    enabled cors to manage origin and handle cookies

--- dist/index.js
+++ dist/index.js
@@ -7,10 +7,15 @@ const express_1 = __importDefault(require("express"));
 const dotenv_1 = __importDefault(require("dotenv"));
 const db_js_1 = __importDefault(require("./config/db.js"));
 const chat_js_1 = __importDefault(require("./routes/chat.js"));
+const cors_1 = __importDefault(require("cors"));
 dotenv_1.default.config();
 const app = (0, express_1.default)();
 (0, db_js_1.default)();
 app.use(express_1.default.json());
+app.use((0, cors_1.default)({
+    origin: "http://localhost:5173",
+    credentials: true,
+}));
 app.use("/api/v1/chat", chat_js_1.default);
 app.listen(process.env.PORT, () => {
     console.log(`Chat service is running on port ${process.env.PORT}`);
