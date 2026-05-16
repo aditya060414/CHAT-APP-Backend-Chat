4bdc53f70fb0457f2dc1a826ec1f5fc3dbf2e0c4
aditya060414 <singh.aditya.44618@gmail.com>
2026-05-16 18:41:21 +0530

    feat(socket): add socket.io server configuration

---
+++ src/config/socket.ts
@@ -0,0 +1,78 @@
+import { Server, Socket } from "socket.io";
+import http from "http";
+import express from "express";
+
+const app = express();
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
+
+const userSocketMap: Record<string, string> = {};
+
+io.on("connection", (socket: Socket) => {
+  console.log("User connected: ", socket.id);
+
+  console.log("query: ", socket.handshake.query);
+  const userId = socket.handshake.query.userId as string | undefined;
+
+  if (userId && userId !== "undefined") {
+    userSocketMap[userId] = socket.id;
+    console.log(`User ${userId} mapped to socket ${socket.id}`);
+  }
+
+  console.log("Current online users:", Object.keys(userSocketMap));
+  io.emit("getOnlineUser", Object.keys(userSocketMap));
+
+  if (userId) {
+    socket.join(userId);
+  }
+
+  socket.on("typing", (data) => {
+    console.log(`User ${data.userId} is typing in chat ${data.chatId}`);
+    socket.to(data.chatId).emit("userTyping", {
+      chatId: data.chatId,
+      userId: data.userId,
+    });
+  });
+  socket.on("stopTyping", (data) => {
+    console.log(`User ${data.userId} stopped typing ${data.chatId}`);
+    socket.to(data.chatId).emit("userStoppedTyping", {
+      chatId: data.chatId,
+      userId: data.userId,
+    });
+  });
+
+  socket.on("joinChat", (chatId) => {
+    socket.join(chatId);
+    console.log(`User ${userId} joined the chat room ${chatId}`);
+  });
+  
+  socket.on("leaveChat", (chatId) => {
+    socket.leave(chatId);
+    console.log(`User ${userId} left the chat room ${chatId}`);
+  });
+  socket.on("disconnect", () => {
+    console.log("User disconnected: ", socket.id);
+    if (userId && userId !== "undefined") {
+      delete userSocketMap[userId];
+      console.log(`User ${userId} removed from online users`);
+      console.log(
+        "Current online users after disconnect:",
+        Object.keys(userSocketMap),
+      );
+      io.emit("getOnlineUser", Object.keys(userSocketMap));
+    }
+  });
+
+  socket.on("connect_error", (error) => {
+    console.log("Socket connection Error, ", error);
+  });
+});
+
+export { app, server, io };
